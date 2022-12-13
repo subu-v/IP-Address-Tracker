8c017c1c823beb45e6e83a4584bab85bbdcd468a
subramani <106590803+subu-v@users.noreply.github.com>
2022-12-13 18:28:31 +0530

    javascript is completed and working.
    1) look ways to optimize js.
    2) did some responsiveness to the project, still not completed

--- src/script.js
+++ src/script.js
@@ -1,18 +1,66 @@
 "use strict";
 
-/* 
+const button = document.querySelector(".tracker__search--button");
+const searchInput = document.querySelector(".tracker__search--input");
+const ipField = document.querySelector(".tracker__data--ip");
+const locationField = document.querySelector(".tracker__data--location");
+const timezoneField = document.querySelector(".tracker__data--timezone");
+const ispField = document.querySelector(".tracker__data--isp");
+let map;
 
+const getUserIpAddress = (async function () {
+  const fetchIP = await fetch("https://api.ipify.org/?format=json");
+  const userIP = await fetchIP.json();
+  renderMap(userIP.ip);
+})();
 
-Your challenge is to build out this IP Address Tracker app and get it looking as close 
-to the design as possible. To get the IP Address locations, you'll be using the
- [IP Geolocation API by IPify](https://geo.ipify.org/). To generate the map, 
- we recommend using [LeafletJS](https://leafletjs.com/).
+const renderData = function (data) {
+  const {
+    ip,
+    location: { city, region, postalCode, timezone },
+    isp,
+  } = data;
+  ipField.textContent = ip;
+  locationField.textContent = `${city}, ${region} ${postalCode}`;
+  timezoneField.textContent = `UTC ${timezone}`;
+  ispField.textContent = isp;
+};
 
-Your users should be able to:
+const renderMarker = function (lat, lng) {
+  const locationIcon = L.icon({
+    iconUrl: require("../images/icon-location.svg"),
+    iconSize: [36, 48],
+  });
 
-- View the optimal layout for each page depending on their device's screen size
-- See hover states for all interactive elements on the page
-- See their own IP address on the map on the initial page load
-- Search for any IP addresses or domains and see the key information and location
+  L.marker([lat, lng], {
+    icon: locationIcon,
+  }).addTo(map);
+};
 
-*/
+const createMap = function (lat, lng) {
+  map = L.map("map", { zoomControl: false }).setView([lat, lng], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+  }).addTo(map);
+};
+
+const renderMap = async function (ip) {
+  const fetchUserInfo = await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_VpIUevGOjDKfNMddcE6asZ7G4HBX5&ipAddress=${ip}`
+  );
+  const retrievedData = await fetchUserInfo.json();
+  renderData(retrievedData);
+
+  const {
+    location: { lat, lng },
+  } = retrievedData;
+
+  createMap(lat, lng);
+  renderMarker(lat, lng);
+};
+
+button.addEventListener("click", function (e) {
+  map.remove();
+  renderMap(searchInput.value);
+});
