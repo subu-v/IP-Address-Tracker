34857123fbd0b19209f53d657f4506c0e54ce84c
subramani <106590803+subu-v@users.noreply.github.com>
2022-12-13 11:59:11 +0530

    UI for desktop screen done.
    1) still not yet responsive.
    2) yet to implement javascript.

---
+++ src/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* 
+
+
+Your challenge is to build out this IP Address Tracker app and get it looking as close 
+to the design as possible. To get the IP Address locations, you'll be using the
+ [IP Geolocation API by IPify](https://geo.ipify.org/). To generate the map, 
+ we recommend using [LeafletJS](https://leafletjs.com/).
+
+Your users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+*/
